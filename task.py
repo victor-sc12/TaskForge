4cee69cb44cecbdc0dc943ddf37140211f1bd62a
Victor Suquilanda <victorsuquilanda8639@outlook.com>
2026-09-07 14:17:42 -0500

    Implementation of simple validations and definition of an iterator for ID creation.

--- task.py
+++ task.py
@@ -4,32 +4,47 @@ import itertools
 
 class Task:
     def __init__(self, id, title):
-        self.id = id
-        self.title = title
-        self.created_at = datetime.now()
-        self.completed = False
+        self._id = id
+        self._title = title
+        self._created_at = datetime.now()
+        self._completed = False
 
     def __str__(self):
-        return f"{self.id}, {self.title}, {self.completed}, {self.created_at}"
+        return f"{self._id}, {self._title}, {self._completed}, {self._created_at}"
+
+    def get_id(self):
+        return self._id
 
     def complete(self):
-        self.completed = True
+        self._completed = True
 
     def rename(self, new_title):
-        self.title = new_title
+        if not new_title:
+            raise ValueError("Invalid title")
+        self._title = new_title
 
 class TaskManager:
-    def __init__(self):
-        self.tasks = []
+    # Initialize a counter starting at 1
+    _id_iter = itertools.count(1)
 
-    def add_task(self, *args):
-        self.tasks.extend(args)
+    def __init__(self):
+        self._tasks = []
+        # self._task_id = next(self._id_iter)
+
+    def _add_task(self, task):
+        self._tasks.append(task)
+
+    def create_task(self, title):
+        task = Task(next(TaskManager._id_iter), title)
+        self._add_task(task)
 
     def list_tasks(self):
-        return self.tasks
+        return self._tasks
 
     def get_task_by_id(self, task_id):
-        task = next((t for t in self.tasks if t.id == task_id), None)
+        task = next((t for t in self._tasks if t.get_id() == task_id), None)
+        if not Task:
+            raise ValueError("Obj not found")
         return task
 
     def complete_task(self, task_id):
