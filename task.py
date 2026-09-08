a7006c428e378327017751523d3be3a67f0323f0
Victor Suquilanda <victorsuquilanda8639@outlook.com>
2026-09-08 13:22:37 -0500

    Key changes: Improved encapsulation in the Task class and distribution of responsibilities.

--- task.py
+++ task.py
@@ -1,59 +1,58 @@
-from abc import ABC, abstractmethod
 from datetime import datetime
 import itertools
 
 class Task:
     def __init__(self, id, title):
-        self._id = id
-        self._title = title
-        self._created_at = datetime.now()
-        self._completed = False
+        self.__id = id
+        self.__title = title
+        self.__created_at = datetime.now()
+        self.__completed = False
 
     def __str__(self):
-        return f"{self._id}, {self._title}, {self._completed}, {self._created_at}"
+        return f"{self.__id}, {self.__title}, {self.__completed}, {self.__created_at}"
 
-    def get_id(self):
-        return self._id
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def title(self):
+        return self.__title
+
+    @title.setter
+    def title(self, title):
+        if not title:
+            raise ValueError("Invalid title")
+
+        self.__title = title
 
     def complete(self):
-        self._completed = True
-
-    def rename(self, new_title):
-        if not new_title:
-            raise ValueError("Invalid title")
-        self._title = new_title
+        if self.__completed:
+            raise ValueError("Task already completed")
+        
+        self.__completed = True
 
 class TaskManager:
     # Initialize a counter starting at 1
     _id_iter = itertools.count(1)
 
     def __init__(self):
-        self._tasks = []
-        # self._task_id = next(self._id_iter)
-
-    def _add_task(self, task):
-        self._tasks.append(task)
+        self.__tasks = []
 
     def create_task(self, title):
         task = Task(next(TaskManager._id_iter), title)
-        self._add_task(task)
-
-    def list_tasks(self):
-        return self._tasks
+        self.__tasks.append(task)
+        return task
 
     def get_task_by_id(self, task_id):
-        task = next((t for t in self._tasks if t.get_id() == task_id), None)
-        if not Task:
+        task = next((t for t in self.__tasks if t.id == task_id), None)
+        if not task:
             raise ValueError("Obj not found")
         return task
 
-    def complete_task(self, task_id):
-        task = self.get_task_by_id(task_id)
-        task.complete()
-        
     def delete_task(self, task_id):
         task = self.get_task_by_id(task_id)
-        del task
+        self.__tasks.remove(task)
 
 if __name__ == '__main__':
     pass
